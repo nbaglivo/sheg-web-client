b911b3cdbf0f7b5ed78bd03d94e7af1a3f2d3814
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2016-07-30 17:04:52 -0300

    Some personalization to the page theme.

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+import { red500, red700, pinkA200 } from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+const muiTheme = getMuiTheme({
+	palette: {
+		primary1Color: red700,
+		primary2Color: red500,
+		secondary1Color: pinkA200,
+	},
+	appBar: {
+		height: 75,
+	},
+});
 
 const App = ({ pushPath, children }) => (
 	<div>
-		<MuiThemeProvider>
+		<MuiThemeProvider muiTheme={muiTheme}>
 		    <main>      
 			  	<AppBar title="Pasae Client" iconClassNameRight="muidocs-icon-navigation-expand-more"/>
 			    {children}
