2cabf961382b299536295dd32adbd82c7c85a37f
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2016-08-07 11:43:56 -0300

    Adding some design to the home page.

--- src/components/userhome.jsx
+++ src/components/userhome.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import FlatButton from 'material-ui/FlatButton';
+
+const style = {
+	appbar: {
+		boxShadow: 'none'
+	}
+}
 
 const UserHome = () => (
-	<div>TODO: UserHome Page</div>
+	<div>
+		<AppBar title="Pasae Cli" style={style.appbar} iconElementRight={<FlatButton label="Logout" />}/>
+		<Tabs>
+			<Tab label="Perfil" >
+				<div>TODO Perfil</div>
+			</Tab>
+			<Tab label="Enfermedad">
+				<div>TODO Enfermedad</div>
+			</Tab>
+		</Tabs>
+	</div>
 )
 
 export default UserHome;
